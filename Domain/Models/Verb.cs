080be3bbacb14c8357fdc8cbe640f74f246c095b
Don Rolling <don.rolling@caliberfs.com>
2023-02-15 16:03:34 -0600

    comments

--- Domain/Models/Verb.cs
+++ Domain/Models/Verb.cs
@@ -4,12 +4,14 @@
 	{
 		public Name Name { get; set; }
 
+		// method parameters
 		public List<Property> Parameters { get; set; } = new List<Property>();
 
 		public bool ParametersAreEmpty { get => Parameters == null || !Parameters.Any(); }
 		
 		public bool ParametersAreNotEmpty { get => Parameters != null && Parameters.Any(); }
 
+		// path parameters
 		public List<Property> PathParameters { get; set; } = new List<Property>();
 
 		public bool PathParametersAreEmpty { get => PathParameters == null || !PathParameters.Any(); }
